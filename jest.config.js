3bcd1852e7981227378c94dce64f74c01626e49c
KaSaNaa <pasindudeshitha123@gmail.com>
2025-04-30 21:26:32 +0530

    fix: update testMatch pattern in jest.config.js to correct path

--- jest.config.js
+++ jest.config.js
@@ -2,16 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: [
-    "**/TSP/src/tests/**/*.test.ts"
+    "**/src/tests/**/*.test.ts"
   ],
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
-  transformIgnorePatterns: [],
-  globals: {
-    'ts-jest': {
-      useESM: true
-    }
-  }
 };
